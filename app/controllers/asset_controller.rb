ed7bafbc12c66197b7c2c9b838752dc67e396a86
manarahmedshehata <manarahmedshehata@gmail.com>
2018-07-01 15:23:56 +0200

    add asset update_price api

--- app/controllers/asset_controller.rb
+++ app/controllers/asset_controller.rb
@@ -1,25 +1,31 @@
 class AssetController < ApplicationController
   def add
   	@asset = Asset.new(asset_params)
+    # check if asset saving is done or not
   	if @asset.save
       render json: {status: "success",asset: @asset}
     else
-      render json: {status: "error"}
+      render json: {status: "error", msg: "Can't save this asset"}
     end
   end
 
   def update_price
-    @asset=Asset.find(params[:name])
-    if params[:price]
-      if @asset.update(price: params[:price])
+    @asset=Asset.find_by_name(asset_params[:name])
+    #Check if price is sent in request body or not
+    if asset_params[:price]
+      # check if asset update is done or not 
+      if @asset.update(price: asset_params[:price])
         render json: {status: "success",asset: @asset}
       else
-        render json: {status: "error"}
+        render json: {status: "error", msg: "Can't update this asset"}
+      end
     else
       render json: {status: "error",msg: "there is no price sent to update "}
+    end
   end
 
   def list
+
   end
 
   def get
